00564581b28e48701376552c514efb659dc94486
dekamahesvara <dekayatha30@gmail.com>
2023-11-04 23:15:36 +0700

    feat: [DK] adding routes

---
+++ lib/common/routes/app_routes.dart
@@ -0,0 +1,10 @@
+abstract class Routes {
+  Routes._();
+  static const splashscreenpage = '/';
+  static const signinpage = '/signin';
+  static const signuppage = '/signup';
+  static const homepage = '/home';
+  static const cartpage = '/cart';
+  static const wishlistpage = '/wishlist';
+  static const paymentpage = '/payment';
+}
